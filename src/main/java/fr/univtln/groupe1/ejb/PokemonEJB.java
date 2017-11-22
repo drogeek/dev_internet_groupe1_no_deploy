1233b3de233fa0c245e303fe85f3775a277672e5
Deshors <yann.deshors@gmail.com>
2017-11-22 16:50:59 +0100

    Correction web.xml

--- src/main/java/fr/univtln/groupe1/ejb/PokemonEJB.java
+++ src/main/java/fr/univtln/groupe1/ejb/PokemonEJB.java
@@ -1,20 +1,25 @@
-package fr.univtln.groupe1.ejb;
+package fr.univtln.groupe1.metier;
 
 
 import fr.univtln.groupe1.metier.Pokemon;
 
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
+import javax.ws.rs.Path;
+
 
 @javax.persistence.Table(name = "Pokemon", catalog = "db1", schema = "public")
 @Stateless
+@Path("/resources")
 public class PokemonEJB {
 
     @PersistenceUnit(unitName = "db1")
     private EntityManagerFactory emf;
 
+    @Path("ejb")
     public Pokemon addPokemon(Pokemon pokemon){
         EntityManager em = emf.createEntityManager();
         em.persist(pokemon);
