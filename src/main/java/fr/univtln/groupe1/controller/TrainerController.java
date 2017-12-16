fe4737bfd3d0b16b0429a7ae22830b2e52d53548
Deshors <yann.deshors@gmail.com>
2017-12-16 17:56:59 +0100

    Fausse connexion, listage des pokemon par utilisateur, ajout pokemon par utilisateur, deconnection - Scope à vérifiergit status!

--- src/main/java/fr/univtln/groupe1/controller/TrainerController.java
+++ src/main/java/fr/univtln/groupe1/controller/TrainerController.java
@@ -1,23 +1,41 @@
 package fr.univtln.groupe1.controller;
 
 import fr.univtln.groupe1.ejb.TrainerEJB;
+import fr.univtln.groupe1.metier.Pokemon;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Named
-@RequestScoped
+@ApplicationScoped
 public class TrainerController {
 
     @Inject
     private TrainerEJB trainerEJB;
+    private List<Pokemon> pokemonList = new ArrayList<>();
 
+    @SessionScoped
     private String sTrainerId ="";
+
+    @RequestScoped
     private String sPokemonName = "";
 
-    public void createPokemon(){
+    public String createPokemon(){
         trainerEJB.addPokemonTrainer(Integer.valueOf(sTrainerId), sPokemonName);
+        getPokemons();
+        return "add_accepted";
+    }
+
+    public void getPokemons (){
+        pokemonList = trainerEJB.listPokemon(Integer.valueOf(sTrainerId));
     }
 
     public String getsTrainerId() {
@@ -34,5 +52,34 @@ public class TrainerController {
 
     public void setsPokemonName(String sPokemonName) {
         this.sPokemonName = sPokemonName;
+    }
+
+    public List<Pokemon> getPokemonList() {
+        return pokemonList;
+    }
+
+
+//    A mettre dans un autre BEAN!!!
+    public void logout() throws IOException {
+
+//        Par securité
+        sTrainerId = "";
+
+//        Invalide la session et retour au depart
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+        ec.invalidateSession();
+        ec.redirect(ec.getRequestContextPath() + "/connexion.xhtml");
+    }
+
+//   Il faudrait une vérification du mot de passe
+    public String connexion(){
+//        On charge sa liste de pokemon
+        getPokemons();
+//        Normalement on check le mot de passe
+        return "accepted";
+    }
+
+    public String add_pokemon_click(){
+        return "createPokemon";
     }
 }
